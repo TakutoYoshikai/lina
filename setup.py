9b4871df00f2425ab3ead11ff8e8c4311f27ce8f
lucky <takuto.yoshikai@gmail.com>
2020-12-02 18:17:29 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = 'Lina',
+    version = '1.0.0',
+    url = 'https://github.com/TakutoYoshikai/lina.git',
+    license = 'MIT LICENSE',
+    author = 'Takuto Yoshikai',
+    author_email = 'takuto.yoshikai@gmail.com',
+    description = 'lina is a steganography program. It can hide a binary file or a text file into multiple png images.',
+    install_requires = ['setuptools', "pycrypto"],
+    packages = find_packages(where="src"),
+    package_dir={"":"src"},
+)
