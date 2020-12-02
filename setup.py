6d153c0c530e96555efb3ecc2e6761ee68af1533
lucky <takuto.yoshikai@gmail.com>
2020-12-02 18:28:47 +0900

    tm9p

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'Lina',
+    name = 'lina',
     version = '1.0.0',
     url = 'https://github.com/TakutoYoshikai/lina.git',
     license = 'MIT LICENSE',
